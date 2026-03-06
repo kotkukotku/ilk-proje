d5f5f028f948ad0fbf356637086a5e581ddb6b83
kotkukotku <veyselerkam@gmail.com>
2026-03-07 02:25:26 +0300

    ThreadPoolExecutor eklendi ve koda comment satırları eklendi

--- hworld.py
+++ hworld.py
@@ -1,26 +1,28 @@
+# Use this code for educational purposes, please!
+# I created this code with AI helping.
 import socket
-import threading
+from concurrent.futures import ThreadPoolExecutor
 
 ip = input("IP adresini giriniz: ")
+timeout = int(input("Timeout'ı giriniz(Varsayılan 1): ")) # User enters timeout number
 open_ports = []
 threads = []
 def port_scan(ip,p):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(5) # I've increased timeout
+        s.settimeout(timeout) 
         try:
             s.connect((ip,p))
             print(f"{p} AÇIK!")
             open_ports.append(p)
-        except:
+        except (TimeoutError, ConnectionRefusedError):
             pass
+        except Exception as e:
+            print("Hata: ", e)
         finally:
             s.close()
-# I've added Threading
-for p in range(1,1001):
-    thread = threading.Thread(target=port_scan,args=(ip,p))
-    threads.append(thread)
-    thread.start()
-for thread in threads:
-     thread.join()
+# I've added ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=50) as executor:
+    for p in range(1,1001):
+        executor.submit(port_scan, ip, p)
 print("AÇIK PORTLAR: ", sorted(open_ports)) # I've sorted all ports
 
